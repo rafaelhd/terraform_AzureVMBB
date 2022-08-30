d4e363a085936e6cb56be3c88bd0932beb723ed5
rafaelhd <rafaelhd89@hotmail.com>
2022-08-30 16:10:29 -0500

    Update azure_vm.tf

--- azure_vm.tf
+++ azure_vm.tf
@@ -1,3 +1,18 @@
+#Configure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
 # Configure the Microsoft Azure provider
   provider "azurerm" {
   client_id = ${var.az_client_id}
