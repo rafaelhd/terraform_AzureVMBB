033ce74485e1db7184f7ef2e1ee1ea3e3fbe43ad
Rafael Hernandez <rafaelhd89@hotmail.com>
2022-08-30 17:22:04 -0500

    add variables

---
+++ variables.tf
@@ -0,0 +1,20 @@
+
+variable "client_id" {
+    type = string
+    description = "required SP client id"
+}
+
+variable "client_secret" {
+    type = string
+    description = "required SP client secret id"
+}
+
+variable "subscription_id" {
+    type = string
+    description = "required SP sub id"
+}
+
+variable "tenant_id" {
+    type = string
+    description = "required SP tenant id"
+}
