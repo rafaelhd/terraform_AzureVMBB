96bf26125408661f23ac166767fcdd0426712df2
Rafael Hernandez <rafaelhd89@hotmail.com>
2022-08-30 18:31:29 -0500

    stuff

--- azure_vm.tf
+++ azure_vm.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_virtual_machine" "tfexample" {
   name                            = "my-terraform-vm"
   location                        = azurerm_resource_group.tfexample.location
   resource_group_name             = azurerm_resource_group.tfexample.name
-  network_interface_ids           = [azurerm_network_interface.tfexample.id]
+#  network_interface_ids           = [azurerm_network_interface.tfexample.id]
   size                            = "Standard_DS1_v2"
   computer_name                   = "myvm"
   admin_username                  = "azureuser"
