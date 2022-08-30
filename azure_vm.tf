a9b2b2ae99ac357b5fa1b1c33d0853fd15ca990d
Rafael Hernandez <rafaelhd89@hotmail.com>
2022-08-30 18:30:35 -0500

    stuff

--- azure_vm.tf
+++ azure_vm.tf
@@ -44,17 +44,17 @@ resource "azurerm_virtual_network" "tfexample" {
 }
 
 # Create a Network Interface
-resource "azurerm_network_interface" "tfexample" {
-  name                = "my-terraform-nic"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+#resource "azurerm_network_interface" "tfexample" {
+#  name                = "my-terraform-nic"
+#  location            = azurerm_resource_group.tfexample.location
+#  resource_group_name = azurerm_resource_group.tfexample.name
 
-  ip_configuration {
-    name                          = "my-terraform-nic-ip-config"
-    subnet_id                     = azurerm_subnet.tfexample.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#  ip_configuration {
+#    name                          = "my-terraform-nic-ip-config"
+#    subnet_id                     = azurerm_subnet.tfexample.id
+#    private_ip_address_allocation = "Dynamic"
+#  }
+#}
 
 # Create a Virtual Machine
 resource "azurerm_linux_virtual_machine" "tfexample" {
