32506a05e8f3dc2870bd73b28a82d16b1a11491f
Rafael Hernandez <rafaelhd89@hotmail.com>
2022-08-30 17:13:12 -0500

    add variables

--- azure_vm.tf
+++ azure_vm.tf
@@ -1,15 +1,17 @@
 # Configure the Microsoft Azure provider
   provider "azurerm" {
-  client_id = ""
-  client_secret = ""
-  subscription_id = ""
-  tenant_id = ""
+    features{
+    }
+    client_id = var.client_id
+    client_secret = var.client_secret
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
 }
 
 # Create a Resource Group if it doesn’t exist
 resource "azurerm_resource_group" "tfexample" {
   name     = "my-terraform-rg"
-  location = "West Europe"
+  location = "westus"
 }
 
 # Create a Virtual Network
