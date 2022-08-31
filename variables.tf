6dbf9ba6c129eee11c7b151f1201591ebcfa4fb5
rafaelhd <rafaelhd89@hotmail.com>
2022-08-31 18:16:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,19 +2,23 @@
 variable "az_client_id" {
     type = string
     description = "required SP client id"
+    default = null
 }
 
 variable "client_secret" {
     type = string
     description = "required SP client secret id"
+    default = null
 }
 
 variable "az_sub_id" {
     type = string
     description = "required SP sub id"
+    default = null
 }
 
 variable "az_tenant_id" {
     type = string
     description = "required SP tenant id"
+    default = null
 }
